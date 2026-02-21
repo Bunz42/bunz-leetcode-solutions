791ce2fd79de23ce8ac98cf22d9f2f8531f738c4
Raymond Hao <156132683+Bunz42@users.noreply.github.com>
2026-02-21 16:53:23 -0500

    Add solution for trapping rainwater problem
    Implement trapping rainwater algorithm using two pointers.

--- neetcode-150/Two Pointers/14-Trapping-Rain-Water/main.py
+++ neetcode-150/Two Pointers/14-Trapping-Rain-Water/main.py
@@ -1,1 +1,21 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        l = 0
+        r = len(height) - 1
+        trapped_water = 0
 
+        tallest_left = height[l]
+        tallest_right = height[r]
+
+        while l < r:
+            if tallest_left < tallest_right:
+                l += 1
+                tallest_left = max(tallest_left, height[l])
+                trapped_water += tallest_left - height[l]
+            
+            else:
+                r -= 1
+                tallest_right = max(tallest_right, height[r])
+                trapped_water += tallest_right - height[r]
+        
+        return trapped_water
