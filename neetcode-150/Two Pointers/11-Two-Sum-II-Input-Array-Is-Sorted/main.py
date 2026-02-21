e471497bc7b5c3f670a0e3ce8ba5e9526e62062b
Raymond Hao <156132683+Bunz42@users.noreply.github.com>
2026-02-21 16:36:06 -0500

    Implement twoSum function using two pointers

--- neetcode-150/Two Pointers/11-Two-Sum-II-Input-Array-Is-Sorted/main.py
+++ neetcode-150/Two Pointers/11-Two-Sum-II-Input-Array-Is-Sorted/main.py
@@ -1,1 +1,14 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        p1 = 0
+        p2 = len(numbers) - 1
+        sum = numbers[p1] + numbers[p2]
 
+        while sum != target:
+            if sum < target:
+                p1 += 1
+            else: 
+                p2 -= 1
+            sum = numbers[p1] + numbers[p2]
+        
+        return [p1+1, p2+1]
