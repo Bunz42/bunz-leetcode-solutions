a0175af1c0a7082680bfa46652f31cd83c59207f
Raymond Hao <156132683+Bunz42@users.noreply.github.com>
2026-02-21 16:53:40 -0500

    Implement maxArea function for container problem

--- neetcode-150/Two Pointers/13-Container-With-Most-Water/main.py
+++ neetcode-150/Two Pointers/13-Container-With-Most-Water/main.py
@@ -1,1 +1,17 @@
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        j = 0
+        k = len(heights) - 1
+        max_area = min(heights[j], heights[k]) * (k - j)
 
+        while j < k:
+            if heights[j] < heights[k]:
+                j += 1
+            else:
+                k -= 1
+            
+            curr_area = min(heights[j], heights[k]) * (k - j)
+            if curr_area > max_area:
+                max_area = curr_area
+        
+        return max_area
