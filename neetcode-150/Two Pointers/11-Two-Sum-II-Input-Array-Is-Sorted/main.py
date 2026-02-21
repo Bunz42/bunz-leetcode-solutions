45e17fb1cd3ab503541ae0d5ee93f25e5b1e049c
Raymond Hao <156132683+Bunz42@users.noreply.github.com>
2026-02-21 16:26:57 -0500

    Remove isPalindrome method from Solution class
    Removed the isPalindrome method from the Solution class.

--- neetcode-150/Two Pointers/11-Two-Sum-II-Input-Array-Is-Sorted/main.py
+++ neetcode-150/Two Pointers/11-Two-Sum-II-Input-Array-Is-Sorted/main.py
@@ -1,14 +1,1 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        new_str = ''.join(c for c in s if c.isalnum()).lower()
-        
-        p1 = 0
-        p2 = len(new_str) - 1
 
-        while p1 < p2:
-            if(new_str[p1] != new_str[p2]):
-                return False
-            p1 += 1
-            p2 -= 1
-        
-        return True
