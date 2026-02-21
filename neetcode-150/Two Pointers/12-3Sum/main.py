4568b196fdc6b53b68a515fc3d9c12eb0df40890
Raymond Hao <156132683+Bunz42@users.noreply.github.com>
2026-02-21 16:47:17 -0500

    Implement threeSum function using two pointers

--- neetcode-150/Two Pointers/12-3Sum/main.py
+++ neetcode-150/Two Pointers/12-3Sum/main.py
@@ -1,1 +1,31 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        result = []
+        
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            target = -nums[i]
+            j = i + 1
+            k = len(nums) - 1
 
+            while j < k:
+                sum = nums[j] + nums[k]
+                if sum < target:
+                    j += 1
+                elif sum > target:
+                    k -= 1
+                else:
+                    result.append([nums[i], nums[j], nums[k]])
+                    
+                    j += 1
+                    k -= 1
+                    
+                    while j < k and nums[j] == nums[j - 1]:
+                        j += 1
+                    
+                    while j < k and nums[k] == nums[k + 1]:
+                        k -= 1
+
+        return result
